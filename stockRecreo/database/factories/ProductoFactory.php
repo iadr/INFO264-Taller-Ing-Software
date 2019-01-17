2f6bd3bb18668af68fb3949ac47f4d4534240cad
iadr <israel.diaz@alumnos.uach.cl>
2019-01-17 15:54:12 -0300

    modificando factorias

--- stockRecreo/database/factories/ProductoFactory.php
+++ stockRecreo/database/factories/ProductoFactory.php
@@ -3,14 +3,14 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Producto::class, function (Faker $faker) {
-  $array=array('juegos de mesa','cartas','magia','circo');
+  $array=array('juegos de mesa','cartas','magia','circo','didacticos','exterior','puzzle','otros','coleccionable');
     return [
       "codigo"=>$faker->ean13,
       "nombre"=>$faker->word,
       "categoria"=>$faker->randomElement($array),
-      "edadminima"=>$faker->numberBetween(2,16),
-      "precio"=>$faker->randomNumber(5),
-      "stock"=>$faker->randomNumber(3),
+      "edadminima"=>$faker->numberBetween(2,18),
+      "precio"=>$faker->numberBetween(1000,150000),
+      "stock"=>$faker->randomNumber(2),
       "activo"=>True
     ];
 });
