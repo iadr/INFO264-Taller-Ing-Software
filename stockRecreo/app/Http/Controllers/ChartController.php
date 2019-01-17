df203247db3fbf6a122e19059deb23d1328ea5f6
turrin <andrew_7653@hotmail.com>
2019-01-17 16:53:00 -0300

    ednaljdbfkahdbs

--- stockRecreo/app/Http/Controllers/ChartController.php
+++ stockRecreo/app/Http/Controllers/ChartController.php
@@ -10,6 +10,29 @@ class ChartController extends Controller
 {
   public function graf1()
     {
+<<<<<<< HEAD
+    	$fechaactual= date('Y-m-d');
+    	$fecha_30_Dias_antes=date('Y-m-d', strtotime('-30 days'));
+
+  
+    $ventas_del_dia = DB::table('ventas')->select(DB::raw('created_at as fecha, monto'))->whereBetween('created_at', array($fechaactual, $fechaactual.' 23:59:59'))->orderBy('created_at','DESC')->get();
+
+   $ventas_totales_por_dias =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->whereBetween('created_at', array($fecha_30_Dias_antes , $fechaactual.' 23:59:59'))->groupBy('fecha')->get();
+
+
+//4 productos mas vendidos
+    $top_mas_vendidos=DB::table('ventas')->join('detalle_ventas','ventas.id','=','detalle_ventas.id_venta')->join('productos','productos.id','=','detalle_ventas.id_producto')->select(DB::raw('productos.nombre as nombre, SUM( detalle_ventas.cantidad)as num_prod'))->whereBetween('ventas.created_at', array($fecha_30_Dias_antes , $fechaactual.' 23:59:59'))->groupBy('nombre')->orderBy('num_prod','DESC')->LIMIT('5')->get(); 
+
+	$total_ventas_30_dias=DB::table('ventas')->select(DB::raw('sum(monto) as total_vendido'))->whereBetween('created_at', array($fecha_30_Dias_antes , $fechaactual.' 23:59:59'))->get();
+
+	$prod_vendidos_en_periodo= DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, count(*) as Ventas'))->whereBetween('created_at', array($fecha_30_Dias_antes , $fechaactual.' 23:59:59'))->groupBy('fecha')->orderBy('fecha','DESC')->get();
+
+   $comparacion =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->whereBetween('created_at', array($fecha_30_Dias_antes , $fechaactual.' 23:59:59'))->groupBy('fecha')->get();
+
+return view('vistaDeEstadisticas',['vd'=> $ventas_del_dia,'vpd'=>$ventas_totales_por_dias, 'top'=>$top_mas_vendidos, 'total'=>$total_ventas_30_dias, 'CatProd'=>$prod_vendidos_en_periodo, 'comparar'=>$comparacion]);
+
+
+=======
 
       $gg =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->groupBy('fecha')->get();
 
@@ -18,21 +41,56 @@ class ChartController extends Controller
 
      $g=DB::table('ventas')->select(DB::raw('sum(monto) as total_vendido'))->whereBetween('created_at', array('2019-01-10' , '2019-01-12 23:59:59'))->get();
       return view('vistaDeEstadisticas',['g'=> $g,'gg'=>$gg, 'v'=>$v]);
+>>>>>>> 3d042bbafb71b83b9c2afef0a42a2e80ee2ce66c
 
     }
 
 
 
+  public function graf2(Request $request)
+  	{
+
+
+
+    $fechaInicio = $request->input('desde');
+    $fechaTermino = $request->input('hasta');
 
 
 
 
+<<<<<<< HEAD
 
-public function graf2(Request $request){
 
-    	$fechaInicio = $request->input('desde');
-    	$fechaTermino = $request->input('hasta');
+     $ventas_del_dia = DB::table('ventas')->select(DB::raw('created_at as fecha, monto'))->whereBetween('created_at', array($fechaInicio.' 00:00:01',$fechaTermino.' 23:59:59'))->orderBy('created_at','asc')->get();
 
+   $ventas_totales_por_dias =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->whereBetween('created_at', array($fechaInicio ,  $fechaTermino.' 23:59:59'))->groupBy('fecha')->get();
+
+//4 productos mas vendidos
+    $top_mas_vendidos=DB::table('ventas')->join('detalle_ventas','ventas.id','=','detalle_ventas.id_venta')->join('productos','productos.id','=','detalle_ventas.id_producto')->select(DB::raw('productos.nombre as nombre, SUM( detalle_ventas.cantidad)as num_prod'))->whereBetween('ventas.created_at', array($fechaInicio, $fechaTermino.' 23:59:59'))->groupBy('nombre')->orderBy('num_prod','DESC')->LIMIT('5')->get(); 
+
+	$total_ventas_30_dias=DB::table('ventas')->select(DB::raw('sum(monto) as total_vendido'))->whereBetween('created_at', array( $fechaInicio,$fechaTermino.' 23:59:59'))->get();
+
+	$prod_vendidos_en_periodo= DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, count(*) as Ventas'))->whereBetween('created_at', array($fechaInicio  ,$fechaTermino.' 23:59:59'))->groupBy('fecha')->orderBy('fecha','asc')->get();
+
+
+
+ //$comparacion =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->where('DATE(created_at)',' =', '2019-01-15')->where(DB::raw('DATE(created_at)',' =', '2019-01-17'))->groupBy('created_at')->get();
+
+
+
+$compA =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->whereBetween('created_at', array($fechaInicio ,$fechaInicio.' 23:59:59'))->groupBy('fecha')->get();
+$compB =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->whereBetween('created_at', array($fechaTermino ,$fechaTermino.' 23:59:59'))->groupBy('fecha')->get();
+$comparacion = array($compA,$compB);
+
+
+
+	return view('vistaDeEstadisticas',['vd'=> $ventas_del_dia,'vpd'=>$ventas_totales_por_dias, 'top'=>$top_mas_vendidos, 'total'=>$total_ventas_30_dias, 'CatProd'=>$prod_vendidos_en_periodo, 'comparar'=>$comparacion]);
+
+
+
+
+//return $comparacion;
+=======
     	$gg = DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha',' monto'))->where('created_at', '>',$fechaInicio)->get();
     //	  $g = DB::table('productos')->select('nombre','stock')->where('categoria','circo')->where('activo', True)->get();
 
@@ -40,10 +98,10 @@ public function graf2(Request $request){
 
 //4 productos mas vendidos
      $v=DB::table('ventas')->join('detalle_ventas','ventas.id','=','detalle_ventas.id_venta')->join('productos','productos.id','=','detalle_ventas.id_producto')->select(DB::raw('productos.nombre as nombre, SUM( detalle_ventas.cantidad)as num_prod'))->groupBy('nombre')->orderBy('num_prod','DESC')->LIMIT('4')->get();
+>>>>>>> 3d042bbafb71b83b9c2afef0a42a2e80ee2ce66c
 
-//$vd=DB::table('ventas')->select(DB::raw('sum(monto) as total_vendido'))->whereBetween('created_at', '2019-01-10 AND 2019-01-13')->get();
 
-      return view('vistaDeEstadisticas',['g'=> $g,'gg'=>$gg, 'v'=>$v]);
 
     }
+
 }
--- stockRecreo/resources/views/vistaDeEstadisticas.blade.php
+++ stockRecreo/resources/views/vistaDeEstadisticas.blade.php
@@ -6,71 +6,30 @@
 <html>
   <head>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    
 
     <script type="text/javascript">
-      google.charts.load('current', {'packages':['bar']});
+      google.charts.load('current', {'packages':['corechart']});
       google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
         var data = google.visualization.arrayToDataTable([
-          ['productos','cantidad vendida'],
-           @foreach($v as $vs)
-            ['{{ $vs->nombre}}',{{$vs->num_prod}}],
+          ['fecha','monto'],
+          @foreach($vpd as $vxd)
+            ['{{ $vxd->fecha}}',{{$vxd->monto}}],
           @endforeach
         ]);
 
         var options = {
+<<<<<<< HEAD
+          title: 'total ventas ultimos 30 días',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+=======
           chart: {
             title: 'Productos mas vendidos',
           }
-        };
-
-        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
-
-        chart.draw(data, google.charts.Bar.convertOptions(options));
-      }
-    </script>
-
-
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-          ['fecha','monto'],
-          @foreach($gg as $gss)
-            ['{{ $gss->fecha}}',{{$gss->monto}}],
-          @endforeach
-        ]);
-
-        var options = {
-          title: 'My Daily Activities'
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-      }
-    </script>
-
-    <script type="text/javascript">
-      google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['fecha','monto'],
-          @foreach($gg as $ggs)
-            ['{{ $ggs->fecha}}',{{$ggs->monto}}],
-          @endforeach
-        ]);
-
-        var options = {
-          title: 'ventas del mes',
-          curveType: 'function',
-          legend: { position: 'bottom' }
+>>>>>>> 3d042bbafb71b83b9c2afef0a42a2e80ee2ce66c
         };
 
         var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
@@ -78,6 +37,84 @@
         chart.draw(data, options);
       }
     </script>
+
+
+
+     <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['fecha','monto'],
+          @foreach($vd as $vdd)
+            ['{{ $vdd->fecha}}',{{$vdd->monto}}],
+          @endforeach
+        ]);
+
+        var options = {
+          title: 'ventas del día',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart 2'));
+
+        chart.draw(data, options);
+      }
+    </script>
+
+ <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['fecha','cantida vendida'],
+          @foreach($CatProd as $cp)
+            ['{{ $cp->fecha}}',{{$cp->Ventas}}],
+          @endforeach
+        ]);
+
+        var options = {
+          title: 'cantidad de productos vendidos en el periodo',
+          curveType: 'function',
+          legend: { position: 'bottom' }
+        };
+
+        var chart = new google.visualization.LineChart(document.getElementById('curve_chart 3'));
+
+        chart.draw(data, options);
+      }
+    </script>
+
+<script type="text/javascript">
+      google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawStuff);
+
+      function drawStuff() {
+        var data = new google.visualization.arrayToDataTable([
+          ['fecha', 'ventas'],
+          @foreach($comparar as $comp)
+            ['{{ $comp->fecha}}',{{$comp->monto}}],
+          @endforeach
+        ]);
+        var options = {
+          width: 800,
+          legend: { position: 'none' },
+          chart: {
+            title: 'comparacion de ventas',
+            
+          bar: { groupWidth: "90%" }
+        }};
+
+        var chart = new google.charts.Bar(document.getElementById('top_x_div'));
+        // Convert the Classic options to Material options.
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      };
+    </script>
+
+
   </head>
   <body>
 
@@ -87,12 +124,20 @@
         {{csrf_field()}}
 
       <label for="bday">Desde:</label>
+<<<<<<< HEAD
+      <input type="date" id="aday" name="desde" required value="<?php echo date('Y-m-d', strtotime('-30 days'));?>"> 
+   
+      <label for="bday">Hasta:</label>
+      <input type="date" id="bday" name="hasta" value="<?php echo date('Y-m-d'); ?>">
+     
+=======
       <input type="date" id="aday" name="desde" required>
 
       <label for="bday">Hasta:</label>
       <input type="date" id="bday" name="hasta" value="<?php echo date('Y-m-d');?>">
 
 
+>>>>>>> 3d042bbafb71b83b9c2afef0a42a2e80ee2ce66c
 
         <button type="submit"  class="btn btn-primary mb-2 btn-success " >Generar nuevas Estadisticas</button>
       </form>
@@ -102,6 +147,11 @@
 
       <div class="">
       <div class="table-responsive">
+<<<<<<< HEAD
+        
+       
+        <h1>5 Productos más vendidos</h1>
+=======
          @foreach($g as $gk)
 
          <h1>{{$gk->total_vendido}}</h1>
@@ -109,6 +159,7 @@
           @endforeach
 
         <h1>Productos más vendidos</h1>
+>>>>>>> 3d042bbafb71b83b9c2afef0a42a2e80ee2ce66c
         <table class="table table-sm table-hover" BORDER=5>
           <thead>
             <tr>
@@ -116,20 +167,43 @@
               <th>Cantidad</th>
             </tr>
           </thead>
-          @foreach($v as $vs)
-            <td>{{ $vs->nombre}}</td>
-            <td>{{$vs->num_prod}}</td>
+          @foreach($top as $top5)
+            <td>{{ $top5->nombre}}</td>
+            <td>{{$top5->num_prod}}</td>
           </tr>
            @endforeach
          </table>
+         @foreach($total as $total)
+         <h1>Total vendido a la fecha ${{ $total->total_vendido}}
+            
+         </h1>
+          @endforeach
       </div>
+<<<<<<< HEAD
+  </div>     
+     
+
+
+   
+=======
   </div>
 
      <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
     <div id="piechart" style="width: 900px; height: 500px;"></div>
+>>>>>>> 3d042bbafb71b83b9c2afef0a42a2e80ee2ce66c
   </body>
   <body>
+
     <div id="curve_chart" style="width: 900px; height: 500px"></div>
+    <div id="curve_chart 2" style="width: 900px; height: 500px"></div>
+     <div id="curve_chart 3" style="width: 900px; height: 500px"></div>
+  <div id="top_x_div" style="width: 800px; height: 600px;"></div>
+
+
+
+
+
+
   </body>
 
 </html>
