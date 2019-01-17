bb6694efe21600c173ed9d11fdcbe52c9cab199a
turrin <andrew_7653@hotmail.com>
2019-01-17 18:17:07 -0300

    chao estadisticas

--- stockRecreo/app/Http/Controllers/ChartController.php
+++ stockRecreo/app/Http/Controllers/ChartController.php
@@ -10,7 +10,6 @@ class ChartController extends Controller
 {
   public function graf1()
     {
-<<<<<<< HEAD
     	$fechaactual= date('Y-m-d');
     	$fecha_30_Dias_antes=date('Y-m-d', strtotime('-30 days'));
 
@@ -27,21 +26,10 @@ class ChartController extends Controller
 
 	$prod_vendidos_en_periodo= DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, count(*) as Ventas'))->whereBetween('created_at', array($fecha_30_Dias_antes , $fechaactual.' 23:59:59'))->groupBy('fecha')->orderBy('fecha','DESC')->get();
 
-   $comparacion =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->whereBetween('created_at', array($fecha_30_Dias_antes , $fechaactual.' 23:59:59'))->groupBy('fecha')->get();
 
-return view('vistaDeEstadisticas',['vd'=> $ventas_del_dia,'vpd'=>$ventas_totales_por_dias, 'top'=>$top_mas_vendidos, 'total'=>$total_ventas_30_dias, 'CatProd'=>$prod_vendidos_en_periodo, 'comparar'=>$comparacion]);
+return view('vistaDeEstadisticas',['vd'=> $ventas_del_dia,'vpd'=>$ventas_totales_por_dias, 'top'=>$top_mas_vendidos, 'total'=>$total_ventas_30_dias, 'CatProd'=>$prod_vendidos_en_periodo]);
 
 
-=======
-
-      $gg =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->groupBy('fecha')->get();
-
-//4 productos mas vendidos
-     $v=DB::table('ventas')->join('detalle_ventas','ventas.id','=','detalle_ventas.id_venta')->join('productos','productos.id','=','detalle_ventas.id_producto')->select(DB::raw('productos.nombre as nombre, SUM( detalle_ventas.cantidad)as num_prod'))->groupBy('nombre')->orderBy('num_prod','DESC')->LIMIT('5')->get();
-
-     $g=DB::table('ventas')->select(DB::raw('sum(monto) as total_vendido'))->whereBetween('created_at', array('2019-01-10' , '2019-01-12 23:59:59'))->get();
-      return view('vistaDeEstadisticas',['g'=> $g,'gg'=>$gg, 'v'=>$v]);
->>>>>>> 3d042bbafb71b83b9c2afef0a42a2e80ee2ce66c
 
     }
 
@@ -58,9 +46,6 @@ return view('vistaDeEstadisticas',['vd'=> $ventas_del_dia,'vpd'=>$ventas_totales
 
 
 
-<<<<<<< HEAD
-
-
      $ventas_del_dia = DB::table('ventas')->select(DB::raw('created_at as fecha, monto'))->whereBetween('created_at', array($fechaInicio.' 00:00:01',$fechaTermino.' 23:59:59'))->orderBy('created_at','asc')->get();
 
    $ventas_totales_por_dias =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->whereBetween('created_at', array($fechaInicio ,  $fechaTermino.' 23:59:59'))->groupBy('fecha')->get();
@@ -73,32 +58,8 @@ return view('vistaDeEstadisticas',['vd'=> $ventas_del_dia,'vpd'=>$ventas_totales
 	$prod_vendidos_en_periodo= DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, count(*) as Ventas'))->whereBetween('created_at', array($fechaInicio  ,$fechaTermino.' 23:59:59'))->groupBy('fecha')->orderBy('fecha','asc')->get();
 
 
+	return view('vistaDeEstadisticas',['vd'=> $ventas_del_dia,'vpd'=>$ventas_totales_por_dias, 'top'=>$top_mas_vendidos, 'total'=>$total_ventas_30_dias, 'CatProd'=>$prod_vendidos_en_periodo]);
 
- //$comparacion =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->where('DATE(created_at)',' =', '2019-01-15')->where(DB::raw('DATE(created_at)',' =', '2019-01-17'))->groupBy('created_at')->get();
-
-
-
-$compA =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->whereBetween('created_at', array($fechaInicio ,$fechaInicio.' 23:59:59'))->groupBy('fecha')->get();
-$compB =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->whereBetween('created_at', array($fechaTermino ,$fechaTermino.' 23:59:59'))->groupBy('fecha')->get();
-$comparacion = array($compA,$compB);
-
-
-
-	return view('vistaDeEstadisticas',['vd'=> $ventas_del_dia,'vpd'=>$ventas_totales_por_dias, 'top'=>$top_mas_vendidos, 'total'=>$total_ventas_30_dias, 'CatProd'=>$prod_vendidos_en_periodo, 'comparar'=>$comparacion]);
-
-
-
-
-//return $comparacion;
-=======
-    	$gg = DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha',' monto'))->where('created_at', '>',$fechaInicio)->get();
-    //	  $g = DB::table('productos')->select('nombre','stock')->where('categoria','circo')->where('activo', True)->get();
-
-      $g =DB::table('ventas')->select(DB::raw('DATE(created_at) as fecha, SUM(monto) as monto'))->groupBy('fecha')->get();
-
-//4 productos mas vendidos
-     $v=DB::table('ventas')->join('detalle_ventas','ventas.id','=','detalle_ventas.id_venta')->join('productos','productos.id','=','detalle_ventas.id_producto')->select(DB::raw('productos.nombre as nombre, SUM( detalle_ventas.cantidad)as num_prod'))->groupBy('nombre')->orderBy('num_prod','DESC')->LIMIT('4')->get();
->>>>>>> 3d042bbafb71b83b9c2afef0a42a2e80ee2ce66c
 
 
 
--- stockRecreo/resources/views/vistaDeEstadisticas.blade.php
+++ stockRecreo/resources/views/vistaDeEstadisticas.blade.php
@@ -21,15 +21,11 @@
         ]);
 
         var options = {
-<<<<<<< HEAD
+
           title: 'total ventas ultimos 30 días',
           curveType: 'function',
           legend: { position: 'bottom' }
-=======
-          chart: {
-            title: 'Productos mas vendidos',
-          }
->>>>>>> 3d042bbafb71b83b9c2afef0a42a2e80ee2ce66c
+
         };
 
         var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
@@ -88,31 +84,7 @@
       }
     </script>
 
-<script type="text/javascript">
-      google.charts.load('current', {'packages':['bar']});
-      google.charts.setOnLoadCallback(drawStuff);
 
-      function drawStuff() {
-        var data = new google.visualization.arrayToDataTable([
-          ['fecha', 'ventas'],
-          @foreach($comparar as $comp)
-            ['{{ $comp->fecha}}',{{$comp->monto}}],
-          @endforeach
-        ]);
-        var options = {
-          width: 800,
-          legend: { position: 'none' },
-          chart: {
-            title: 'comparacion de ventas',
-            
-          bar: { groupWidth: "90%" }
-        }};
-
-        var chart = new google.charts.Bar(document.getElementById('top_x_div'));
-        // Convert the Classic options to Material options.
-        chart.draw(data, google.charts.Bar.convertOptions(options));
-      };
-    </script>
 
 
   </head>
@@ -124,20 +96,11 @@
         {{csrf_field()}}
 
       <label for="bday">Desde:</label>
-<<<<<<< HEAD
-      <input type="date" id="aday" name="desde" required value="<?php echo date('Y-m-d', strtotime('-30 days'));?>"> 
-   
-      <label for="bday">Hasta:</label>
-      <input type="date" id="bday" name="hasta" value="<?php echo date('Y-m-d'); ?>">
-     
-=======
       <input type="date" id="aday" name="desde" required>
 
       <label for="bday">Hasta:</label>
       <input type="date" id="bday" name="hasta" value="<?php echo date('Y-m-d');?>">
 
-
->>>>>>> 3d042bbafb71b83b9c2afef0a42a2e80ee2ce66c
 
         <button type="submit"  class="btn btn-primary mb-2 btn-success " >Generar nuevas Estadisticas</button>
       </form>
@@ -147,19 +110,11 @@
 
       <div class="">
       <div class="table-responsive">
-<<<<<<< HEAD
+
         
        
         <h1>5 Productos más vendidos</h1>
-=======
-         @foreach($g as $gk)
 
-         <h1>{{$gk->total_vendido}}</h1>
-
-          @endforeach
-
-        <h1>Productos más vendidos</h1>
->>>>>>> 3d042bbafb71b83b9c2afef0a42a2e80ee2ce66c
         <table class="table table-sm table-hover" BORDER=5>
           <thead>
             <tr>
@@ -179,25 +134,16 @@
          </h1>
           @endforeach
       </div>
-<<<<<<< HEAD
+
   </div>     
      
 
-
-   
-=======
-  </div>
-
-     <div id="columnchart_material" style="width: 800px; height: 500px;"></div>
-    <div id="piechart" style="width: 900px; height: 500px;"></div>
->>>>>>> 3d042bbafb71b83b9c2afef0a42a2e80ee2ce66c
   </body>
   <body>
 
     <div id="curve_chart" style="width: 900px; height: 500px"></div>
     <div id="curve_chart 2" style="width: 900px; height: 500px"></div>
      <div id="curve_chart 3" style="width: 900px; height: 500px"></div>
-  <div id="top_x_div" style="width: 800px; height: 600px;"></div>
 
 
 
